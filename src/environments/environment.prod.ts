4bf01173a239c9e0ac014dfc11a28ce6325551c5
the-markussohn <markus.justinas@gmail.com>
2018-08-23 22:25:04 +0300

    firestore imports

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAhKBn3nmBFtwLtFxIaHLjmh2wAhoexopU',
+    authDomain: 'clientpanelprod-e2676.firebaseapp.com',
+    databaseURL: 'https://clientpanelprod-e2676.firebaseio.com',
+    projectId: 'clientpanelprod-e2676',
+    storageBucket: 'clientpanelprod-e2676.appspot.com',
+    messagingSenderId: '923809326057'
+  }
 };
